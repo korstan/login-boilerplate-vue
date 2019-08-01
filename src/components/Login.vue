1db222dbd19720354bae222f15688c758f13933b
stanislavkorda <stanislav.korda@saritasa.com>
2019-08-01 10:58:06 +0500

    login form layout

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login-form">
+    <h1>{{ msg }}</h1>
+    <label for="input-login">Login</label>
+    <input id="input-login" type="text">
+    <label for="input-password">Password</label>
+    <input id="input-password" type="text">
+    <button type="submit">Sign In</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login-form {
+  max-width: 270px;
+  margin: 0 auto;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 10px solid #42b983;
+  border-radius: 5%
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
